f5c32d5ffe2a8cd40dee8020324784bae060c99f
Jesufemi-A <jesufemia1@gmail.com>
2024-06-30 14:04:20 -0700

    Initial commit

---
+++ src/test/java/com/example/HNG_basic_web_server/HngBasicWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HNG_basic_web_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HngBasicWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
