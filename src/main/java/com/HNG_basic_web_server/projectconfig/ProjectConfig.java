3fe4ea61b564c927b8df4a9b6b15e512eca717be
Jesufemi-A <jesufemia1@gmail.com>
2024-07-01 06:55:10 -0700

    project configurarition

--- src/main/java/com/HNG_basic_web_server/projectconfig/ProjectConfig.java
+++ src/main/java/com/HNG_basic_web_server/projectconfig/ProjectConfig.java
@@ -1,2 +1,9 @@
-package com.HNG_basic_web_server.projectconfig;public class ProjectConfig {
+package com.HNG_basic_web_server.projectconfig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.HNG_basic_web_server.restcontroller"})
+public class ProjectConfig {
 }
